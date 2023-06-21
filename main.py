8b58960ae7f6a64ada0ffa50a31a9c228e0595e2
alixdecr <56295260+alixdecr@users.noreply.github.com>
2023-06-21 18:41:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def main():
     logger.logMessage("MutGPT - The Automatic API Request Mutation and Specification Inferring Tool", "title", "blue", True)
     print("")
 
-    # STEP 1: ASK FOR WEB SERVICE DETAILS
-    name = "GBIF Species API"
-    apiKey = "" # leave empty if the API does not require a key
-    openaiKey = "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"
+    # STEP 1: ASK FOR API DETAILS
+    name = input("API Name: ")
+    apiKey = input("API Key (if the API does not require a key, press ENTER): ") # leave empty if the API does not require a key
+    openaiKey = input("OpenAI Key: ")
+    nbMutations = int(input("Number of Mutations: "))
     model = "text-davinci-003"
     base = "" # leave empty for attempt to find a valid API base
     seed = "" # leave empty for attempt to find a valid seed
-    nbMutations = 20
     seedLimiting = False
 
     # STEP 2: INSTANTIATE THE MUTATION AND REQUEST GENERATOR CLASS
